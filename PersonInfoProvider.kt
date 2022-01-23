950855a4e762af0a94b06b0bc55a07990ae1f121
Rafael Martins <rafael.dev101@gmail.com>
2022-01-23 20:15:45 -0300

    interfaces intermediate check implemetation of interface

--- PersonInfoProvider.kt
+++ PersonInfoProvider.kt
@@ -1,10 +1,43 @@
 interface PersonInfoProvider {
-    fun printInfoInterface(person: PersonConstructors)
+
+    val providerInfo: String
+
+    fun printInfoInterface(person: PersonConstructors){
+        println(providerInfo)
+        person.printInfo()
+    }
 }
 
-class BasicInfoProvider: PersonInfoProvider{
+interface anotherInterface{
+
+    fun printSomething(){
+        println("another Interface used")
+    }
+
+}
+
+interface andAnotherInterface{
+    fun andprintSomething(){
+        println("and another Interface used")
+    }
+}
+
+class BasicInfoProvider: PersonInfoProvider, anotherInterface,andAnotherInterface{
+    override val providerInfo: String
+        get() = "not override function"
+
     override fun printInfoInterface(person: PersonConstructors) {
-        println("override method saying something")
+        super.printInfoInterface(person)
+
+        println("more thing to do in override function")
+    }
+}
+
+fun checkInterface(personInfoProvider: PersonInfoProvider){
+    if(personInfoProvider is anotherInterface){
+        println("it is a anotherInterface")
+    }else{
+        println("it is not a anotherInterface")
     }
 }
 
@@ -13,5 +46,10 @@ fun main(){
     val provider = BasicInfoProvider()
 
     provider.printInfoInterface(PersonConstructors())
+
+    provider.printSomething()
+    provider.andprintSomething()
+
+    checkInterface(provider)
 
 }
