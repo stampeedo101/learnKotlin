e538c3cca8a3e7b931e08d5e2140601ff3dc0f42
Rafael Martins <rafael.dev101@gmail.com>
2022-01-23 16:04:41 -0300

    null values and functional program

--- Main.kt
+++ Main.kt
@@ -1,7 +1,29 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    //val is final
+    val name: String = "Rufus"
+
+    println("name = $name")
+
+    //var is variable
+    var nameR: String = "Catherine"
+
+    nameR = "Josephine"
+
+    println("nameR = $nameR")
+
+    //defaul variable in Kotlin don't have null value
+    //so you need "?"
+    var name_null: String? = null
+
+    name_null = "void"
+
+    when(name_null){
+        null -> println("null")
+        else -> println(name_null)
+    }
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    println("Program arguments: ${args.joinToString()}")
 }
