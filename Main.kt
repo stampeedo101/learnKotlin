997fbb43d7d658b4d498e0b623645feaf2d92365
Rafael Martins <rafael.dev101@gmail.com>
2022-01-23 17:48:11 -0300

    array and:
    forEach,
    forEachIndexed,

--- Main.kt
+++ Main.kt
@@ -50,6 +50,17 @@ fun main(args: Array<String>) {
     println(arrayOfThings.get(1))
     println(arrayOfThings.get(2))
 
+    arrayOfThings.forEach {
+        println("for each arrayOfThings = " + it)
+    }
+
+    arrayOfThings.forEachIndexed { index, arrayOfThings ->
+        println("from index[" + index + "] arrayOfThings = " + arrayOfThings)
+
+    }
+
+
+
     //FUNCTIONAL PROGRAM
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
